9aa871e807dc56d73181b22aae696d9f5bef3810
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2025-01-02 14:08:28 -0600

    Create files and functions for add, subtract, multiply and divide

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNums(a,b){
+    return a+b;
+}
+
+function subtractNums(a,b){
+    return a-b;
+}
+
+function multiplyNums(a,b){
+    return a*b;
+}
+
+function divideNums(a,b){
+    return a/b;
+}
