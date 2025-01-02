617e301cafccb3dd1f8970f107e483682097a571
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2025-01-02 15:52:26 -0600

    Finish styling (layout) of calculator on web page

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiplyNums(a,b){
 function divideNums(a,b){
     return a/b;
 }
+
+function operate(num1, num2, operation){
+    let result; 
+    if(operation==="add"){ //not sure what operation values are yet
+        result = addNums(num1, num2);
+    }else if(operation==="subtract"){
+        result = subtractNums(num1, num2);
+    }else if(operation==="multiply"){
+        result = multiplyNums(num1, num2);
+    }else if(operation==="divide"){
+        result = divideNums(num1, num2);
+    }
+
+    return result;
+}
